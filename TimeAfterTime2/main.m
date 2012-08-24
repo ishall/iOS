d810b5f880beecca30a8e337733b4bdf3e768a53
ishall <ishall@live.com>
2012-08-24 14:41:42 -0400

    date functions

--- TimeAfterTime2/main.m
+++ TimeAfterTime2/main.m
@@ -16,7 +16,39 @@ int main(int argc, const char * argv[])
         NSDate *now = [NSDate date];
         NSLog(@"The date now is %p", now);
         NSLog (@"the date now is %@", now);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog (@"the date later will be %@", later);
+        
+        //set the first date
+        NSDateComponents *firstDate = [[NSDateComponents alloc]init];
+        [firstDate setYear:2011];
+        [firstDate setMonth:6];
+        [firstDate setDay:8];
+        [firstDate setHour:10];
+        
+        NSCalendar *greg = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
+        
+        NSDate *initDate = [greg dateFromComponents:firstDate];
+        
+        //set the next date
+        
+        NSDateComponents *secondDate = [[NSDateComponents alloc]init];
+        [secondDate setYear:2012];
+        [secondDate setMonth:6];
+        [secondDate setDay:8];
+        [secondDate setHour:10];
+                
+        NSDate *laterDate = [greg dateFromComponents:secondDate];
+        
+        //difference between the two dates
+        
+        double diffDate = [laterDate timeIntervalSinceDate:initDate];
+        
+        NSLog(@"the difference is %f", diffDate);
+        
 
+        
         
     }
     return 0;
