fc30b3aadc0df1770564ce1abe1a8208d17da1cb
ishall live <ishall@live.com>
2012-08-24 10:20:41 -0400

    Initial Commit

---
+++ TimeAfterTime2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime2
+//
+//  Created by somename on 8/24/12.
+//  Copyright (c) 2012 somename. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
